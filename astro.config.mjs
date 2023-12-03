e98567c359b6657d06aedddcb466223eed800810
pawan67 <pawantamada8@gmail.com>
2023-12-03 23:19:33 +0530

    configured for vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
 import node from "@astrojs/node";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
   output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
+  adapter: vercel(),
 });

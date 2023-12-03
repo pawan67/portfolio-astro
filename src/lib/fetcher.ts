4ea95cebba81da898023f3f9ba0f416cfda021d4
pawan67 <pawantamada8@gmail.com>
2023-12-03 23:22:56 +0530

    fixed spotify api

--- src/lib/fetcher.ts
+++ src/lib/fetcher.ts
@@ -2,7 +2,7 @@ import type { Track } from "@/types/spotify";
 
 export async function recentTrack() {
   const res = await fetch(
-    `http://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=pawan67&api_key=${
+    `https://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=pawan67&api_key=${
       import.meta.env.PUBLIC_LAST_FM_API_KEY
     }&format=json`
   );
